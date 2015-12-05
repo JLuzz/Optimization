2dfa5e89e5be8c918c335da6a1ed1eb76287e04b
Jeremy Luzzi <jluzz403@gmail.com>
2015-12-05 14:09:26 -0700

    can now take in all command line args

--- main.c
+++ main.c
@@ -1,80 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char chunkID[4];
-int chunkSize;
-char format[4];
+struct waveData{
 
-char subChunk1ID[4];
-int subChunk1Size;
-short audioFormat;
-short numChannels;
-int sampleRate;
-int byteRate;
-short blockAlign;
-short bitsPerSample;
+	int valid;
 
-int channelSize;
-char subChunk2ID[4];
-int subChunk2Size;
+	char chunkID[4];
+	int chunkSize;
+	char format[4];
 
-short* data;
+	char subChunk1ID[4];
+	int subChunk1Size;
+	short audioFormat;
+	short numChannels;
+	int sampleRate;
+	int byteRate;
+	short blockAlign;
+	short bitsPerSample;
 
-void print()
+	int channelSize;
+	char subChunk2ID[4];
+	int subChunk2Size;
+
+	short* data;
+
+};
+
+void printWave(struct waveData waveD)
 {
 	printf("\n============= HEADER INFO =============\n");
-	printf(" chunkID:%s\n", chunkID);
-	printf(" chunkSize:%d\n", chunkSize);
-	printf(" format:%s\n", format);
-	printf(" subChunk1ID:%s\n", subChunk1ID);
-	printf(" subChunk1Size:%d\n", subChunk1Size);
-	printf(" audioFormat:%d\n", audioFormat);
-	printf(" numChannels:%d\n", numChannels);
-	printf(" sampleRate:%d\n", sampleRate);
-	printf(" byteRate:%d\n", byteRate);
-	printf(" blockAlign:%d\n", blockAlign);
-	printf(" bitsPerSample:%d\n", bitsPerSample);
-	printf(" subChunk2ID:%s\n", subChunk2ID);
-	printf(" subChunk2Size:%d\n", subChunk2Size);
+	printf(" chunkID:%s\n", waveD.chunkID);
+	printf(" chunkSize:%d\n", waveD.chunkSize);
+	printf(" format:%s\n", waveD.format);
+	printf(" subChunk1ID:%s\n", waveD.subChunk1ID);
+	printf(" subChunk1Size:%d\n", waveD.subChunk1Size);
+	printf(" audioFormat:%d\n", waveD.audioFormat);
+	printf(" numChannels:%d\n", waveD.numChannels);
+	printf(" sampleRate:%d\n", waveD.sampleRate);
+	printf(" byteRate:%d\n", waveD.byteRate);
+	printf(" blockAlign:%d\n", waveD.blockAlign);
+	printf(" bitsPerSample:%d\n", waveD.bitsPerSample);
+	printf(" subChunk2ID:%s\n", waveD.subChunk2ID);
+	printf(" subChunk2Size:%d\n", waveD.subChunk2Size);
 }
 
-int loadWave(char* filename)
+struct waveData loadWave(char* filename)
 {
 	FILE* in = fopen(filename, "rb");
+	struct waveData wave;
 
 	if (in != NULL)
 	{
+
 		printf("Reading %s...\n",filename);
 
-		fread(chunkID, 1, 4, in);
-		fread(&chunkSize, 1, 4, in);
-		fread(format, 1, 4, in);
+		fread(wave.chunkID, 1, 4, in);
+		fread(&wave.chunkSize, 1, 4, in);
+		fread(wave.format, 1, 4, in);
 
 		//sub chunk 1
-		fread(subChunk1ID, 1, 4, in);
-		fread(&subChunk1Size, 1, 4, in);
-		fread(&audioFormat, 1, 2, in);
-		fread(&numChannels, 1, 2, in);
-		fread(&sampleRate, 1, 4, in);
-		fread(&byteRate, 1, 4, in);
-		fread(&blockAlign, 1, 2, in);
-		fread(&bitsPerSample, 1, 2, in);
+		fread(wave.subChunk1ID, 1, 4, in);
+		fread(&wave.subChunk1Size, 1, 4, in);
+		fread(&wave.audioFormat, 1, 2, in);
+		fread(&wave.numChannels, 1, 2, in);
+		fread(&wave.sampleRate, 1, 4, in);
+		fread(&wave.byteRate, 1, 4, in);
+		fread(&wave.blockAlign, 1, 2, in);
+		fread(&wave.bitsPerSample, 1, 2, in);
 
 		//read extra bytes
-		if(subChunk1Size == 18)
+		if(wave.subChunk1Size == 18)
 		{
 			short empty;
 			fread(&empty, 1, 2, in);
 		}
 
 		//sub chunk 2
-		fread(subChunk2ID, 1, 4, in);
-		fread(&subChunk2Size, 1, 4, in);
+		fread(wave.subChunk2ID, 1, 4, in);
+		fread(&wave.subChunk2Size, 1, 4, in);
 
 		//read data
-		int bytesPerSample = bitsPerSample/8;
-		int numSamples = subChunk2Size / bytesPerSample;
-		data = (short*) malloc(sizeof(short) * numSamples);
+		int bytesPerSample = wave.bitsPerSample/8;
+		int numSamples = wave.subChunk2Size / bytesPerSample;
+		wave.data = (short*) malloc(sizeof(short) * numSamples);
 
 		//fread(data, 1, bytesPerSample*numSamples, in);
 
@@ -82,7 +90,7 @@ int loadWave(char* filename)
 		short sample=0;
 		while(fread(&sample, 1, bytesPerSample, in) == bytesPerSample)
 		{
-			data[i++] = sample;
+			wave.data[i++] = sample;
 			sample = 0;
 		}
 
@@ -92,12 +100,15 @@ int loadWave(char* filename)
 	else
 	{
 		printf("Can't open file\n");
-		return 0;
+		wave.valid = 0;
+		return wave;
 	}
-	return 1;
+
+	wave.valid = 1;
+	return wave;
 }
 
-int saveWave(char* filename)
+int saveWave(struct waveData wave, char* filename)
 {
 	FILE* out = fopen(filename, "wb");
 
@@ -105,43 +116,43 @@ int saveWave(char* filename)
 	{
 		printf("\nWriting %s...\n",filename);
 
-		fwrite(chunkID, 1, 4, out);
-		fwrite(&chunkSize, 1, 4, out);
-		fwrite(format, 1, 4, out);
+		fwrite(wave.chunkID, 1, 4, out);
+		fwrite(&wave.chunkSize, 1, 4, out);
+		fwrite(wave.format, 1, 4, out);
 
 		//sub chunk 1
-		fwrite(subChunk1ID, 1, 4, out);
-		fwrite(&subChunk1Size, 1, 4, out);
-		fwrite(&audioFormat, 1, 2, out);
-		fwrite(&numChannels, 1, 2, out);
-		fwrite(&sampleRate, 1, 4, out);
-		fwrite(&byteRate, 1, 4, out);
-		fwrite(&blockAlign, 1, 2, out);
-		fwrite(&bitsPerSample, 1, 2, out);
+		fwrite(wave.subChunk1ID, 1, 4, out);
+		fwrite(&wave.subChunk1Size, 1, 4, out);
+		fwrite(&wave.audioFormat, 1, 2, out);
+		fwrite(&wave.numChannels, 1, 2, out);
+		fwrite(&wave.sampleRate, 1, 4, out);
+		fwrite(&wave.byteRate, 1, 4, out);
+		fwrite(&wave.blockAlign, 1, 2, out);
+		fwrite(&wave.bitsPerSample, 1, 2, out);
 
 		//read extra bytes
-		if(subChunk1Size == 18)
+		if(wave.subChunk1Size == 18)
 		{
 			short empty = 0;
 			fwrite(&empty, 1, 2, out);
 		}
 
 		//sub chunk 2
-		fwrite(subChunk2ID, 1, 4, out);
-		fwrite(&subChunk2Size, 1, 4, out);
+		fwrite(wave.subChunk2ID, 1, 4, out);
+		fwrite(&wave.subChunk2Size, 1, 4, out);
 
 		//read data
-		int bytesPerSample = bitsPerSample / 8;
-		int sampleCount =  subChunk2Size / bytesPerSample;
+		int bytesPerSample = wave.bitsPerSample / 8;
+		int sampleCount =  wave.subChunk2Size / bytesPerSample;
 
 		//impulse response - echo
 		int IRSize = 6;
 		float IR[IRSize];
 		IR[0] = 0.0;
 		IR[1] = 1.0;
-		IR[2] = -1.0;
+		IR[2] = 1.0;
 		IR[3] = 1.0;
-		IR[4] = -1.0;
+		IR[4] = 1.0;
 		IR[5] = 0.0;
 
 		//write the data
@@ -154,7 +165,7 @@ int saveWave(char* filename)
 
 			//convolve
 			for(int j=0; j<IRSize; ++j)
-				newData[i+j] += ((float)data[i] / MAX_VAL) * IR[j];
+				newData[i+j] += ((float) wave.data[i] / MAX_VAL) * IR[j];
 
 			//Keep track of max value for scaling
 			if(i==0)
@@ -186,13 +197,17 @@ int saveWave(char* filename)
 
 int main(int argc, char* argv[])
 {
-	char* filename = argv[1];
+	char* inSoundWav = argv[1];
+	//char*	IRwav = argv[2];
+	char* outSoundWav = argv[2];
 
-	if(loadWave(filename))
-		print();
+	struct waveData soundIn = loadWave(inSoundWav);
+
+	if(soundIn.valid)
+		printWave(soundIn);
 
 
 
-	saveWave("out.wav");
-	free(data);
+	saveWave(soundIn, outSoundWav);
+	free(soundIn.data);
 }
